1dd31f238f120026d367aa3f3b7e02dd0d6b4f09
Pujanp17 <105946631+Pujanp17@users.noreply.github.com>
2022-07-08 03:42:59 +0530

    Update lab04_Code.py
    printing out the results

--- lab04_Code.py
+++ lab04_Code.py
@@ -8,7 +8,12 @@ header = {'Accept': 'application/json'}
 response_msg=requests.get(url, headers=header)
 
 if response_msg.status_code == requests.codes.ok:
-    response_msg.json()
-    
+    poke_dictionary=response_msg.json()
+
+    print(poke_dictionary ['results'])
+    pass
+else:
+    Print(f'status code: {response_msg.status_code}, Reason of Error occurrence: {response_msg.reason} ')
+
 
 pass
