b1855fb4baa9296d201b7fdb569af2869fe64c58
Pujanp17 <105946631+Pujanp17@users.noreply.github.com>
2022-07-08 08:56:25 +0530

    poki_api successful completed

--- lab04_Code.py
+++ lab04_Code.py
@@ -1,19 +1,24 @@
 import requests
 #Importing required packages.
 
-url = 'https://pokeapi.co/api/v2/ability/'
+    url = 'https://pokeapi.co/api/v2/ability/'
 
-header = {'Accept': 'application/json'}
+    def search_pokeapi(search_term=''):
 
-response_msg=requests.get(url, headers=header)
+    #Cleaning the search term.
+        search_term = str(search_term).strip().lower()
 
-if response_msg.status_code == requests.codes.ok:
-    poke_dictionary=response_msg.json()
+    #assigning value to header parameter to json file.
+        header = {'Accept': 'application/json'}
 
-    print(poke_dictionary ['results'])
-    pass
-else:
-    Print(f'status code: {response_msg.status_code}, Reason of Error occurrence: {response_msg.reason} ')
+    #Assigning parameter to search and response message.
+        search_url = url + 'search'
+        response_msg = requests.get(search_url, headers=header )
 
-
-pass
+    #defining the response message in poke_api dictionary.
+        if response_msg.status_code == requests.codes.ok:
+    #returning to the response message.
+            return response_msg.json()
+        else:
+            Print(f'status code: {response_msg.status_code}, Reason of Error occurrence: {response_msg.reason} ')
+        pass
