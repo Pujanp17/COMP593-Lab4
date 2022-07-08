36cb65df6dd8186388da81b58a63f5ee1179f665
Pujanp17 <105946631+Pujanp17@users.noreply.github.com>
2022-07-08 10:53:45 +0530

    Update pastbin_api.py

--- pastbin_api.py
+++ pastbin_api.py
@@ -1,4 +1,5 @@
-
+import requests
+ 
 def post_new_paste(title, body_text, expirations='N', listed=True)
     """
     posts a new paste to PasteBin
@@ -13,10 +14,19 @@ def post_new_paste(title, body_text, expirations='N', listed=True)
 
     print('posting a new paste to paste bin',end='')
 
+    p = {
+        'api_dev_key': lBf-u7pU3ls6dYLR4eD3EURjDVhhEn3K,
+        'api_option': paste,
+        'api_paste_code': body_text,
+        'api_paste_name':title,
+        'api_paste_private': 0 if listed else 1,
+        'api_paste_expire_date': expiration
+    }
 
-    #Assigning parameter to search and response message.
-        search_url = url + 'search'
-        response_msg = requests.get(search_url)
+
+    #Assigning parameter to search and response message
+        paste_url = 'https://pastebin.com/api/api_post.php',
+        response_msg = requests.post(paste_url, data=p)
 
     #defining the response message in poke_api dictionary.
         if response_msg.status_code == requests.codes.ok:
